b11c5439776a13aed9a756afb958a68abdf24794
Guitar <kittipong.sataya@gmail.com>
2024-09-24 14:17:34 +0700

    v

--- notes-app-front/src/utils/constants.js
+++ notes-app-front/src/utils/constants.js
@@ -1,3 +1,5 @@
-const baseUrl = "http://localhost:5000/api";
+// const baseUrl = "http://localhost:5000/api";
+
+const baseUrl = "https://notes-app-myself.onrender.com/api";
 
 export default baseUrl
